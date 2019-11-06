50c666e2cab3cd38117ef004c723fc47b7ee7619
hieu292 <nguyen.huy.hieu292@gmail.com>
2019-11-06 13:53:02 +0700

    update error

--- lib/messaging.ex
+++ lib/messaging.ex
@@ -50,8 +50,7 @@ defmodule FirebaseAdminEx.Messaging do
          {:ok, body} <- Response.parse(response) do
       {:ok, body}
     else
-      {:error, error} ->
-        raise Errors.ApiError, Kernel.inspect(error)
+      {:error, error} -> {:error, error}
     end
   end
 
